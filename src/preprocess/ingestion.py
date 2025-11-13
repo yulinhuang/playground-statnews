5a82db7f05bed334edff2e25e1ca1f9e5bff6cfb
Yu-Lin Huang <yu-lin.huang@uni.lu>
2025-11-13 16:38:11 +0100

    Add seed in ollama call, add docx as dependancy

--- src/preprocess/ingestion.py
+++ src/preprocess/ingestion.py
@@ -47,3 +47,32 @@ def get_target_index(df: pd.DataFrame, target: float, code: str = '') -> pd.Data
     filtered_idx = df.loc[code] 
     result = filtered_idx[target]
     return float(round(result, 1))
+
+
+def get_top_variations_with_ponderation(df: pd.DataFrame, target: float,  n: int = 5, prefix: str = '', positive: bool = True, level: str = '4') -> pd.DataFrame:
+
+    df_pond = get_sheet("Pondération")
+    filtered_pond = df_pond[df_pond.index.str.startswith(prefix)]
+    filtered_pond = filtered_pond[filtered_pond['Niveau'] == level]
+
+    filtered_idx = df[df.index.str.startswith(prefix)]
+    filtered_idx = filtered_idx[filtered_idx['Niveau'] == level]
+    filtered_idx[target] = filtered_idx[target].round(1)
+
+    ponderated = filtered_idx[target] * filtered_pond[int(target)]
+    # Top n positive values
+    top_n = (
+        ponderated[ponderated > 0].nlargest(n)
+        if positive
+        else ponderated[ponderated < 0].nsmallest(n)
+    )
+
+    # Combine them
+    top_n = top_n.round(1)
+
+    # Convert to dictionary
+    result_dict = {
+        df['description'][coicop]: float(filtered_idx[target].loc[coicop])
+        for coicop in top_n.index
+    }
+    return result_dict
